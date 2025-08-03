0bcae39d5d98bf55159a172c54560390c2dc34d7
wahe7 <waheguru002200@gmail.com>
2025-08-03 13:23:09 +0530

    adding voice route

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
-
+const voiceRoutes = require("./routes/voice");
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
